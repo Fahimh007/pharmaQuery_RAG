f06f07a3e52b0df0b007d144b62afda384fc8d63
Fahimh007 <hasan35-1177@s.diu.edu.bd>
2026-04-01 20:20:56 +0600

    Add missing newline at the beginning of app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
